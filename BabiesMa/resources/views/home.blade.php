48e9e154fa8509c703acfe5c570ec22950525691
As-tou <125726410+As-tou@users.noreply.github.com>
2024-01-30 07:48:55 +0000

    commit2

--- BabiesMa/resources/views/home.blade.php
+++ BabiesMa/resources/views/home.blade.php
@@ -47,20 +47,20 @@
 </html>
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-bottom">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Aujourd'hui</a>
+      <a class="navbar-brand" href="/Aujourd'hui">Aujourd'hui</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Bébé</a>
+            <a class="nav-link active" aria-current="page" href="/Bébé">Bébé</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Moi</a>
+            <a class="nav-link" href="/Moi">Moi</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Plus</a>
+            <a class="nav-link" href="/Plus">Plus</a>
           </li>
         </ul>
       </div>
